043f27aa4144661dc8a13b8034a577c50464ec99
Lissa Urbano <lissaurbano@gmail.com>
2021-04-02 15:52:54 -0300

    Service

---
+++ src/main/java/com/facens/event/services/EventService.java
@@ -0,0 +1,16 @@
+package com.facens.event.services;
+
+import com.facens.event.repositories.EventRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    
+    
+}
