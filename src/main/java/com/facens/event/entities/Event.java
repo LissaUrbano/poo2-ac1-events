bfb814e0ee1190d95402bc8ebdc902e0afe4f010
Lissa Urbano <lissaurbano@gmail.com>
2021-04-02 15:26:55 -0300

    conexão db-H2

--- src/main/java/com/facens/event/entities/Event.java
+++ src/main/java/com/facens/event/entities/Event.java
@@ -1,10 +1,18 @@
 package com.facens.event.entities;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Event {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Event implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
     private Long id;
     private String name;
     private String description;
@@ -84,6 +92,31 @@ public class Event {
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Event other = (Event) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
 }
