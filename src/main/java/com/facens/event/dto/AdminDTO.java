7f1f0908a12e9c626c23319758109c57fa01f528
Lissa Urbano <lissaurbano@gmail.com>
2021-04-27 21:27:08 -0300

    Admin entitie and dto

---
+++ src/main/java/com/facens/event/dto/AdminDTO.java
@@ -0,0 +1,59 @@
+package com.facens.event.dto;
+
+import com.facens.event.entities.Admin;
+
+public class AdminDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    
+    public AdminDTO() {
+    }
+
+    public AdminDTO(Long id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public AdminDTO(Admin admin) {
+        setId(admin.getId());
+        setName(admin.getName());
+        setEmail(admin.getEmail());
+        setEmail(admin.getPhoneNumber());
+	}
+
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
