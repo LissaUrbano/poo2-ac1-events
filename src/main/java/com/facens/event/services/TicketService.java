a06b0da160b9a69029943c57c58831b2d6e940ed
Lissa Urbano <lissaurbano@gmail.com>
2021-06-08 19:18:28 -0300

    Correction TicketService

--- src/main/java/com/facens/event/services/TicketService.java
+++ src/main/java/com/facens/event/services/TicketService.java
@@ -7,8 +7,10 @@ import com.facens.event.repositories.TicketRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+@Service
 public class TicketService {
     
     @Autowired
